ea0b49fa1d36222911619d9643db61a98ededa42
Jeff Wu <wuthefwasthat@gmail.com>
2021-02-28 17:13:49 -0800

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="lesser",
     py_modules=["lesser"],
-    version="0.0.1",
+    version="0.0.2",
     install_requires=[],
     description="Simple version of pandas",
     author="wuthefwasthat",
